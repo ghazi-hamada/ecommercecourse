f2bdbb4b37b6540672254c008863929e5e25a504
Eng. ghazi hamada <ghazihamada7@gmail.com>
2023-05-13 22:41:10 +0300

    product details

--- lib/view/screen/product_details.dart
+++ lib/view/screen/product_details.dart
@@ -12,31 +12,36 @@ class ProductDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = Get.put(ProductDetailsControllerImpl());
     return Scaffold(
-      body: Container(
-        child: ListView(
-          children: [
-            Stack(
-              children: [
-                Container(
-                  height: 200,
-                  decoration: const BoxDecoration(
-                      color: AppColor.primaryColor,
-                      borderRadius:
-                          BorderRadius.vertical(bottom: Radius.circular(20))),
-                ),
-                Positioned(
+      body: ListView(
+        children: [
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                height: 200,
+                decoration: const BoxDecoration(
+                    color: AppColor.primaryColor,
+                    borderRadius:
+                        BorderRadius.vertical(bottom: Radius.circular(20))),
+              ),
+              Positioned(
+                top: 50,
+                right: Get.width / 20,
+                left: Get.width / 20,
+                child: Hero(
+                  tag: controller.itemsModel.itemsId.toString(),
                   child: CachedNetworkImage(
                     imageUrl:
                         "${AppLink.imageItems}/${controller.itemsModel.itemsImage!}",
-                    height: 200,
-                    width: MediaQuery.of(context).size.width,
-                    fit: BoxFit.scaleDown,
+                    height: 250,
+                    fit: BoxFit.fill,
+                    width: Get.width,
                   ),
                 ),
-              ],
-            )
-          ],
-        ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
